ed84f2d1157635e772e251a7d24bacc99c8dfe33
Joseph Skimmons <Joseph.Skimmons@icims.com>
2018-07-02 10:06:11 -0400

    parsed tables from comments

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
 years_list = ["2015", "2016", "2017", "2018"]
 
@@ -30,4 +30,26 @@ for em in soup.find_all("em"):
 		link_list.append(a.get("href"))
 
 print(len(link_list))
+print(link_list[0])
 
+
+f = open("SanFran.txt", "w")
+
+boxScore_url = "https://www.baseball-reference.com{}".format(link_list[0])
+
+
+page = requests.get(boxScore_url).text
+
+com_soup = BeautifulSoup(page, "lxml")
+
+comments=com_soup.find_all(string=lambda text:isinstance(text, Comment))
+
+tables = []
+
+for comment in comments:
+	tmpSoup = BeautifulSoup(str(comment), "lxml")
+	if tmpSoup.find("table"):
+		tables.append(tmpSoup)
+
+# batting in [1] and [2]
+# pitching in [3]
