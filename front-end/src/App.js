ead798055f7efa8ce16353497cfa95ca5d52be5b
Joe Skimmons <jwskimmons@gmail.com>
2018-08-24 12:42:21 -0400

    Improve

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import PageContainer from './games/PageContainer';
+import ApiTestComponent from './games/ApiTestComponent';
 
 
 class App extends Component {
@@ -9,7 +10,10 @@ class App extends Component {
   render() {
     return (
       <Provider store={this.props.store}>
+      	<div>
+              <ApiTestComponent />
               <PageContainer />
+        </div>
       </Provider>
     );
   }
