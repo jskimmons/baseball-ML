1a54d4e236427f00fb3735fc8cbcd666829fbd83
Joe <jwskimmons@gmail.com>
2018-08-26 16:04:44 -0400

    updated url structure

--- site2/urls.py
+++ site2/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('baseball/', include('baseball.urls')),
+	path('', include('baseball.urls')),
     path('admin/', admin.site.urls),
 ]
